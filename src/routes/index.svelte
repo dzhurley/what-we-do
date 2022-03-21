64cbb3d3fef538d318e6ce410d82cee5c6015050
Derek Hurley <dzhurley@gmail.com>
2022-03-21 07:24:26 -0700

    add picking filtering by tag

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,34 @@
 <script lang="ts">
-  import { Button } from 'spaper';
+  import { Button, Checkbox } from 'spaper';
   import confetti from 'canvas-confetti';
 
-  import { things } from '$lib/sanity';
+  import { tags, things } from '$lib/sanity';
   import type { CMSThing } from '$lib/types';
 
   import Thing from '$lib/Thing.svelte';
 
   let choice: CMSThing;
 
+  let filters = [];
+
   const choose = () => {
-    choice = $things[Math.floor(Math.random() * $things.length)];
-    confetti({
-      particleCount: 100,
-      spread: 70,
-      origin: { y: 0.6 },
+    const filtered = $things.filter(thing => {
+      if (!filters.length) {
+        return true;
+      }
+      return filters.every(f => thing.tags.find(t => t.value === f));
     });
+    const newChoice = filtered[Math.floor(Math.random() * filtered.length)];
+
+    choice = newChoice || null;
+
+    if (choice) {
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+      });
+    }
   };
 </script>
 
@@ -23,9 +36,21 @@
   {#if choice}
     <Thing thing={choice} readonly />
     <Button on:click={choose}>Pick another!</Button>
+  {:else if choice === null}
+    <h2>All choices filtered out!</h2>
+    <Button on:click={choose}>Pick another!</Button>
   {:else}
     <Button size="large" on:click={choose}>Pick one!</Button>
   {/if}
+
+  <label
+    >Filters
+    <fieldset class="form-group">
+      {#each $tags as tag}
+        <Checkbox label={tag.label} value={tag.value} bind:group={filters} />
+      {/each}
+    </fieldset>
+  </label>
 </section>
 
 <style>
@@ -33,6 +58,26 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
-    height: 100vh;
+    margin-top: 110px;
+    height: calc(100vh - 110px);
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  fieldset {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+
+  label {
+    margin: 2rem auto;
+    padding: 0 2rem;
+  }
+
+  :global(.form-group .paper-check) {
+    margin-bottom: -0.5rem;
   }
 </style>
